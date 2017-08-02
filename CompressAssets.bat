a53284e6e9e310579f2d203ccce6c13b9f31934e
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2017-08-02 11:17:59 +0200

    * Modified CompressAssets.bat / DecompressAssets.bat scripts to use the Assets.7z names instead of Dependencies.7z filenames. * Removed .assbin assets. This can be recreated by the application and these don't need to be stored in the archive. * Removed the powerplant model. This is a large model and not very interesting to view with this demo.

--- CompressAssets.bat
+++ CompressAssets.bat
@@ -1,6 +1,7 @@
 @echo off
 pushd %~dp0
 
+del /F /Q Assets.7z.*
 .\Tools\7zip\7za.exe a -v500m Assets.7z Assets/models/*
 
 popd
--- DecompressAssets.bat
+++ DecompressAssets.bat
@@ -1,7 +1,7 @@
 @echo off
 pushd %~dp0
 
-.\Tools\7zip\7za.exe x Dependencies.7z.001
+.\Tools\7zip\7za.exe x Assets.7z.001
 
 popd
 
