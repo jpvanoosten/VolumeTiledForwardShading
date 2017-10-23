52073b853b5f76cf491231d926a936ae3c05a8de
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2017-10-23 17:20:10 +0200

    * Do not build boost dependencies (I will provide precompiled binaries with the repo).

--- InitializeRepo.bat
+++ InitializeRepo.bat
@@ -1,15 +1,9 @@
 @ECHO OFF
 PUSHD %~dp0
 
-ECHO Initialize and update submodules (and submodules of submodules)...
+ECHO Initialize and update submodules...
 git submodule update --init --recursive
 ECHO Done initilizing submodules.
-
-PUSHD .\externals\boost\
-CALL .\bootstrap.bat
-CALL .\b2 headers
-CALL .\b2 --with-serialization --stagedir=./ link=static runtime-link=shared threading=multi address-model=64 -j8 stage
-POPD
 POPD
 
 PAUSE
